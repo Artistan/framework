e3c1c06bea7bd867b74ed1e54d2a18c46bb865cc
Charles Peterson <cpeterson@brokerbin.com>
2017-10-04 14:37:43 -0500

    live udpate

--- src/Illuminate/Bus/Queueable.php
+++ src/Illuminate/Bus/Queueable.php
@@ -94,8 +94,10 @@ trait Queueable
     public function dispatchNextJobInChain()
     {
         if (! empty($this->chained)) {
-            dispatch(tap(unserialize(array_shift($this->chained)), function ($next) {
+            dispatch(tap(unserialize(array_shift($this->chained)), function (Queueable $next) {
                 $next->chained = $this->chained;
+                $next->onConnection($next->connection ?: $this->connection);
+                $next->onQueue($next->queue ?: $this->queue);
             }));
         }
     }
