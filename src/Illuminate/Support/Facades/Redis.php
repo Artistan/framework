6d8d656c3086ee7c51a0cee3546fba5b53148f54
Charles Peterson <cpeterson@brokerbin.com>
2017-12-14 15:58:51 -0600

    wow, that is a lot of work. events, listeners, queues, hostnames, environments,.... so much going on

--- src/Illuminate/Support/Facades/Redis.php
+++ src/Illuminate/Support/Facades/Redis.php
@@ -5,6 +5,13 @@ namespace Illuminate\Support\Facades;
 /**
  * @method static \Illuminate\Redis\Connections\Connection connection(string | null $name)
  * @method static \Illuminate\Redis\Connections\Connection resolve(string | null $name)
+ * @method static void createSubscription(array|string $channels, \Closure $callback, string $method = 'subscribe')
+ * @method static \Illuminate\Redis\Limiters\ConcurrencyLimiterBuilder funnel(string $name)
+ * @method static \Illuminate\Redis\Limiters\DurationLimiterBuilder throttle(string $name)
+ * @method static \Predis\Client client()
+ * @method static void subscribe( array|string|null $channels, \Closure $callback)
+ * @method static void psubscribe(array|string|null $channels, \Closure $callback)
+
  * @method static array connections()
  *
  * @see \Illuminate\Redis\RedisManager
